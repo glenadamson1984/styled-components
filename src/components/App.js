78dabd3a923d8b185e614d1bf4817357f0501676
glenadamson1984 <glenadamson1984@gmail.com>
2021-02-28 20:25:10 +0000

    adding button with props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
+import React from 'react';
+import {Button} from 'components/common';
+import {createGlobalStyle} from 'styled-components';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+    body{
+      background: white;
+      min-height: 100vh;
+      margin: 0;
+      color: black;
+      font-family: 'Kaushan Script';
+    }
+`;
+
+const App = () => {
   return (
-    <h1>App</h1>
+    <div>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>Primary</Button>
+      <Button secondary >Secondary</Button>
+      <Button disabled>disabled</Button>
+      <Button large>large</Button>
+    </div>
+
   );
 }
 
---
+++ src/components/common/Button.js
@@ -0,0 +1,29 @@
+import styled, {css} from 'styled-components';
+
+const Button = styled.button`
+    color: white;
+    background: ${props => props.secondary ? 'black' : '#f8049c'};
+    font-weight: bold;
+    box-shadow: none;
+    border: none;
+    width: 100%;
+    display: block;
+    white-space: none;
+
+    ${props => props.large ? css`
+    padding: 16px;
+    border-radius: 8px;
+    font-size: 2em;
+    ` : css`
+    padding: 8px;
+    border-radius: 4px;
+    font-size: 1em;
+    `};
+
+    &:disabled{
+        background: #eee;
+        color: #666;
+    }
+`;
+
+export {Button};
